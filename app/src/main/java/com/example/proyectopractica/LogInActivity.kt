4645cc79b3f89f4d7161aa037d4df7c59a01a981
Awakt-04 <asimay898@alu.centroafuera.es>
2024-02-04 19:33:25 +0100

    Continuación 2 LogInActivity, XML terminado Continuación Firebase, RegisterActivity, PrincipalActivity

--- app/src/main/java/com/example/proyectopractica/LogInActivity.kt
+++ app/src/main/java/com/example/proyectopractica/LogInActivity.kt
@@ -3,21 +3,37 @@ package com.example.proyectopractica
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import com.google.firebase.auth.FirebaseAuth
 
 class LogInActivity : AppCompatActivity() {
 
+    private lateinit var fAuth: FirebaseAuth
+
     private lateinit var registerButton: Button
     private lateinit var loginButton: Button
+    private lateinit var usernameText: EditText
+    private lateinit var passwordText: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
 
+        fAuth = FirebaseAuth.getInstance()
+
         registerButton = findViewById(R.id.registerButton)
         loginButton = findViewById(R.id.logInButton)
+        usernameText = findViewById(R.id.usernameEditText)
+        passwordText = findViewById(R.id.passwordEditText)
 
-        this.register()
-        this.login()
+
+        loginButton.setOnClickListener(){
+            login()
+        }
+
+        register()
+
     }
     
     private fun register(){
@@ -26,8 +42,59 @@ class LogInActivity : AppCompatActivity() {
         }
     }
     private fun login(){
-        loginButton.setOnClickListener(){
-            startActivity(Intent(this,PrincipalActivity::class.java))
+        var name = usernameText.text.toString()
+        var password = passwordText.text.toString()
+        var confirm = true
+
+        if (name.isBlank()) {
+            usernameText.error = getString(R.string.NameError)
+            usernameText.requestFocus()
+            confirm = false
+        }
+
+        confirm = passConfirm(password)
+
+        if (confirm) {
+            Log.i("OK LogIn","Inicio de sesión correcto")
+            startActivity(Intent(this, PrincipalActivity::class.java))
+        }
+        else
+            Log.i("Error LogIn","Inicio de sesión fallido")
+
+
+
+    }
+
+    private fun passConfirm(s: String): Boolean{
+        when{
+            s.isBlank() -> {
+                passwordText.error = getString(R.string.PasswordBlankError)
+                passwordText.requestFocus()
+                return false
+            }
+            s.length < 8 -> {
+                passwordText.error = getString(R.string.PasswordLengthError)
+                passwordText.requestFocus()
+                return false
+            }
+            !s.contains(Regex("[A-Z]")) -> {
+                passwordText.error = getString(R.string.PasswordCapitalError)
+                passwordText.requestFocus()
+                return false
+            }
+
+            !s.contains(Regex("[0-9]")) -> {
+                passwordText.error = getString(R.string.PasswordNumberError)
+                passwordText.requestFocus()
+                return false
+            }
+
+            !s.contains(Regex("[\\W_]")) -> {
+                passwordText.error = getString(R.string.PasswordSimbolError)
+                passwordText.requestFocus()
+                return false
+            }
+            else -> return true
         }
     }
 
--- app/src/main/java/com/example/proyectopractica/PrincipalActivity.kt
+++ app/src/main/java/com/example/proyectopractica/PrincipalActivity.kt
@@ -1,4 +1,11 @@
 package com.example.proyectopractica
 
-class PrincipalActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PrincipalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_principal)
+    }
 }
