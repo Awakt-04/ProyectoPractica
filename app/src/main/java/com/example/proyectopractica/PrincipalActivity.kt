ed74848bb37a4c6facaa65c3315c12a12a950275
Awakt-04 <asimay898@alu.centroafuera.es>
2024-02-08 12:33:12 +0100

    Continuación PrincipalActivity

--- app/src/main/java/com/example/proyectopractica/PrincipalActivity.kt
+++ app/src/main/java/com/example/proyectopractica/PrincipalActivity.kt
@@ -3,6 +3,7 @@ package com.example.proyectopractica
 import android.content.Intent
 import android.content.res.Resources
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.GridView
 import android.widget.ImageButton
@@ -14,6 +15,7 @@ class PrincipalActivity : AppCompatActivity() {
     private lateinit var db :FirebaseFirestore
 
     private lateinit var adaptador : AdapterAvatar
+    private lateinit var spinnerAdapter : ArrayAdapter<*>
     private lateinit var avatarButton : ImageButton
     private lateinit var seriesButton: Button
     private lateinit var moviesButton: Button
@@ -21,12 +23,15 @@ class PrincipalActivity : AppCompatActivity() {
 
     private var item = 0
 
+    private var movies = intArrayOf(R.drawable.pelicula_oppenhaimer,R.drawable.pelicula_sociedad_nieve,
+        R.drawable.pelicula_asesinos_luna)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_principal)
 
         db = FirebaseFirestore.getInstance()
+
 
         avatarButton = findViewById(R.id.avatarButton)
         seriesButton = findViewById(R.id.serieButton)
@@ -51,7 +56,7 @@ class PrincipalActivity : AppCompatActivity() {
         moviesSeries.adapter = adaptador
 
         moviesSeries.setOnItemClickListener { _, _, position, _ ->
-            item = list[position]
+            item = movies[position]
             try {
                 Toast.makeText(this, "Serie seleccionada: ${resources.getResourceEntryName(item)}", Toast.LENGTH_SHORT).show()
 
